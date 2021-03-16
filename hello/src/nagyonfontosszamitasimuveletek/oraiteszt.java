643e90374948d1938dd4daa987884b6142211a0a
Kovacs Sengupta <atthy4@hotmail.com>
2021-03-16 13:16:51 +0100

    asd2
    asd

---
+++ hello/src/nagyonfontosszamitasimuveletek/oraiteszt.java
@@ -0,0 +1,14 @@
+package nagyonfontosszamitasimuveletek;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class oraiteszt {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
